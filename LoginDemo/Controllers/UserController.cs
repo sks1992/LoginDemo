4cf3b70b3dbef5a22dcb23b2e012233fcc13e525
Sandeep <sandeepsk15892>
2023-02-01 16:03:57 +0530

    Add project files.

---
+++ LoginDemo/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using LoginDemo.Data;
+using LoginDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginDemo.Controllers
+{
+	[Route("api/demoData")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly AppDbContext _authContext;
+		public UserController(AppDbContext authContext) 
+		{
+			_authContext= authContext;
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult> Authenticate([FromBody]User user)
+		{
+			if(user == null)
+			{
+				return BadRequest();
+			}
+
+			var userObj =await  _authContext.Users.FirstOrDefaultAsync(u=> u.UserName == user.UserName &&
+			u.Password == user.Password); 
+			if(userObj == null)
+			{
+				return NotFound(new { Message ="User is Not Found!"});
+			}
+			return Ok(new {Message="Login Success"});
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> RegisterUser([FromBody] User  user)
+		{
+			if(user==null)
+			{
+				return BadRequest();	
+			}
+
+			await _authContext.Users.AddAsync(user);
+			await _authContext.SaveChangesAsync();
+			return Ok(new { Message= "User Register Success" });
+		}
+	}
+}
